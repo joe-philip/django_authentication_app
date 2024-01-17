b904704820838db86bbd5a9c0df39d69b6bbb911
Joe Philip <joe.philip@hotmail.co.in>
2024-01-18 01:02:18 +0530

    defined User model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
 
 
@@ -23,3 +25,55 @@ class Nationalities(models.Model):
         db_table = 'nationalities'
         verbose_name = 'Nationality'
         verbose_name_plural = 'Nationalities'
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, mobile, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, full_name=full_name,
+                          mobile=mobile, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, mobile, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email, password, and admin privileges.
+        """
+        extra_fields.setdefault('role', 3)
+        return self.create_user(email, full_name, mobile, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class RoleChoices(models.IntegerChoices):
+        STUDENT = 1, 'Student'
+        STAFF = 2, 'Staff'
+        ADMIN = 3, 'Admin'
+        EDITOR = 4, 'Editor'
+
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.IntegerField(choices=RoleChoices.choices, default=1)
+    country = models.ForeignKey(
+        Countries, on_delete=models.SET_NULL, null=True
+    )
+    nationality = models.ForeignKey(
+        Nationalities, on_delete=models.SET_NULL, null=True
+    )
+    other_country = models.CharField(null=True, max_length=50)
+    other_nationality = models.CharField(null=True, max_length=50)
+    mobile = models.CharField(max_length=50, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name', 'mobile']
+
+    objects = UserManager()
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
