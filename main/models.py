35efa1d4cb9b36b33a7a960a83de510bd5c9a069
Joe Philip <joe.philip@hotmail.co.in>
2024-01-17 23:30:27 +0530

    defined Countries model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Countries(models.Model):
+    name = models.CharField(max_length=50)
+    iso2 = models.CharField(max_length=2)
+    iso3 = models.CharField(max_length=3)
+
+    class Meta:
+        db_table = 'country'
+        verbose_name = 'country'
+        verbose_name_plural = 'countries'
+
