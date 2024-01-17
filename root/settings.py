f89af37d6c68047f46de882d520a392ec38c80f1
Joe Philip <joe.philip@hotmail.co.in>
2024-01-18 01:16:28 +0530

    moved db config inside .env

--- root/settings.py
+++ root/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(default=f'sqlite:////{join(BASE_DIR, "db.sqlite3")}')
 }
 
 
