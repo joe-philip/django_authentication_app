dcd41a4b294920d5b8e8259ae83242e0bfb37678
Joe Philip <joe.philip@hotmail.co.in>
2024-01-19 02:04:04 +0530

    defined LoginView

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import CreateView, TemplateView
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views.generic import CreateView, FormView, TemplateView
 
-from .forms import UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -14,3 +18,19 @@ class UserSignupView(SuccessMessageMixin, CreateView):
     form_class = UserRegistrationForm
     success_url = '/'
     success_message = 'Signup success login to continue'
+
+
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'login.html'
+    success_url = '/'
+
+    def form_valid(self, form: LoginForm) -> HttpResponse:
+        if user := authenticate(self.request, **form.cleaned_data):
+            login(self.request, user=user)
+            return super().form_valid(form)
+        messages.error(
+            request=self.request,
+            message='Login failed, Invalid credentials'
+        )
+        return redirect('/login')
