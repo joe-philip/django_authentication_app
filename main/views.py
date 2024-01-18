f229cffe794295ba3d3f66762783ae1c0f40a67f
Joe Philip <joe.philip@hotmail.co.in>
2024-01-19 01:01:01 +0530

    removed unused imports

--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
-from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
 from django.views.generic import CreateView, TemplateView
 
 from .forms import UserRegistrationForm
