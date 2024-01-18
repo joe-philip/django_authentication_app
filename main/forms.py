f7bcfd1fc2177cd2eafadf2c1d9a95f31b5fddb5
Joe Philip <joe.philip@hotmail.co.in>
2024-01-19 00:50:43 +0530

    defined UserRegistrationForm

--- main/forms.py
+++ main/forms.py
@@ -1,1 +1,69 @@
+from typing import Any
+
 from django import forms
+
+from .models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={'id': 'password'}
+        )
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput(
+            attrs={
+                'id': 'confirm_password'
+            }
+        )
+    )
+    other_country = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'id': 'other_country',
+                'style': 'display: none',
+                'placeholder': 'Enter country'
+            }
+        ),
+        required=False
+    )
+    other_nationality = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'id': 'other_nationality',
+                'style': 'display: none',
+                'placeholder': 'Enter nationality'
+            }
+        ),
+        required=False
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'full_name', 'mobile', 'email', 'country',
+            'nationality', 'other_country', 'other_nationality'
+        )
+        widgets = {
+            'email': forms.EmailInput(attrs={'id': 'email'}),
+            'mobile': forms.TextInput(attrs={'id': 'mobile', 'type': 'tel'}),
+            'country': forms.Select(attrs={'id': 'country', 'onchange': 'checkOther(this)'}),
+            'nationality': forms.Select(attrs={'id': 'nationality', 'onchange': 'checkOther(this)'}),
+            'full_name': forms.TextInput(attrs={'id': 'fullName'})
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+    def save(self, *args, **kwargs) -> Any:
+        self.cleaned_data.pop('password2', '')
+        user: User = User.objects.create_user(**self.cleaned_data)
+        user.set_password(self.cleaned_data.get('password'))
+        user.save()
+        return user
