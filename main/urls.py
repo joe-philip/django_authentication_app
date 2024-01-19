da9db2c6ba91dad0cf8d44da85eba0555314c7da
Joe Philip <joe.philip@hotmail.co.in>
2024-01-19 12:40:30 +0530

    defined url for dashboard view

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup', views.UserSignupView.as_view(), name='signup'),
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
+    path('dashboard', views.DashboardAPI.as_view(), name='dashboard')
 ]
