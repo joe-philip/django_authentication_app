c701e682b25c6bba197bad939c2538af29974572
Joe Philip <joe.philip@hotmail.co.in>
2024-01-19 12:40:12 +0530

    defined DashboardAPI

--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, FormView, TemplateView, View
 
 from .forms import LoginForm, UserRegistrationForm
+from .models import User
 
 
 # Create your views here.
@@ -29,12 +32,28 @@ class LoginView(FormView):
     def form_valid(self, form: LoginForm) -> HttpResponse:
         if user := authenticate(self.request, **form.cleaned_data):
             login(self.request, user=user)
+            messages.success(self.request, 'Logged in successfully')
             return super().form_valid(form)
         messages.error(
             request=self.request,
             message='Login failed, Invalid credentials'
         )
         return redirect('/login')
+
+
+class DashboardAPI(View):
+    @method_decorator(login_required(login_url='/login'))
+    def get(self, request: HttpRequest) -> HttpResponse:
+        match request.user.role:
+            case 1:
+                return render(request, 'dashboard/student.html')
+            case 2:
+                return render(request, 'dashboard/staff.html')
+            case 3:
+                context = {'queryset': User.objects.all().exclude(role=3)}
+                return render(request, 'dashboard/admin.html', context)
+            case 4:
+                return render(request, 'dashboard/editor.html')
 
 
 class LogoutView(View):
